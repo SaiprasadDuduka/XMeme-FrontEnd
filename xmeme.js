34e8fcabf077f515b574d18e7a1dc2a56c43ccb3
Saiprasad Duduka <dsp221099@gmail.com>
2021-02-10 14:33:22 +0530

    Frontend completed successfully

--- xmeme.js
+++ xmeme.js
@@ -3,6 +3,10 @@ var captionInput = document.querySelector("#caption");
 var urlInput = document.querySelector("#url");
 var submitBtn = document.querySelector("#submit");
 var stream = document.querySelector(".bottom");
+var memeID = document.querySelector("#memeID");
+var memeName = document.querySelector("#memeName");
+var memeCaption = document.querySelector("#memeCaption");
+var memeURL = document.querySelector("#memeURL");
 const serverURL = "http://ec2-3-17-231-21.us-east-2.compute.amazonaws.com:3000";
 
 function reset() {
@@ -43,17 +47,14 @@ $("#submit").click(function () {
 
 function addButtonListener() {
     $(".patchB").click(function () {
+        openForm();
         var id = (this.id).slice(0, -1);
-        $.ajax({
-            url: serverURL + "/memes/" + id,
-            type: 'PATCH',
-            data: {
-                caption: "temporary",
-                url: "https://miro.medium.com/max/830/0*HWez4Yya6Cac_2LB.jpg"
-            },
-            success: function (result) {
-                loadMemes();
-            }
+        $.get(serverURL + "/memes/" + id, (data, status) => {
+            var meme = JSON.parse(JSON.stringify(data));
+            memeID.value = id;
+            memeName.value = meme.name;
+            memeCaption.value = meme.caption;
+            memeURL.value = meme.url;
         });
     });
 
@@ -66,6 +67,30 @@ function addButtonListener() {
                 loadMemes();
             }
         });
+    });
+}
+
+function openForm() {
+    document.getElementById("myForm").style.display = "block";
+}
+
+function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+}
+
+function updateContent(){
+    var id = memeID.value;
+    $.ajax({
+        url: serverURL + "/memes/" + id,
+        type: 'PATCH',
+        data: {
+            caption: memeCaption.value,
+            url: memeURL.value
+        },
+        success: function (result) {
+            closeForm();
+            loadMemes();
+        }
     });
 }
 
