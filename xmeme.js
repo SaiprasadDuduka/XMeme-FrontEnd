40ee344cda55aec0b1d8ba0dcb109abe9bc118e1
Saiprasad Duduka <dsp221099@gmail.com>
2021-02-11 23:59:26 +0530

    Validation added successfully

--- xmeme.js
+++ xmeme.js
@@ -33,26 +33,64 @@ function loadMemes() {
     });
 }
 
+//check if given text is url
+function is_url(str) {
+    regexp = /^(?:(?:https?|ftp):\/\/)?(?:(?!(?:10|127)(?:\.\d{1,3}){3})(?!(?:169\.254|192\.168)(?:\.\d{1,3}){2})(?!172\.(?:1[6-9]|2\d|3[0-1])(?:\.\d{1,3}){2})(?:[1-9]\d?|1\d\d|2[01]\d|22[0-3])(?:\.(?:1?\d{1,2}|2[0-4]\d|25[0-5])){2}(?:\.(?:[1-9]\d?|1\d\d|2[0-4]\d|25[0-4]))|(?:(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)(?:\.(?:[a-z\u00a1-\uffff0-9]-*)*[a-z\u00a1-\uffff0-9]+)*(?:\.(?:[a-z\u00a1-\uffff]{2,})))(?::\d{2,5})?(?:\/\S*)?$/;
+    if (regexp.test(str)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+//check if input fields are empty
+function validate() {
+    if (nameInput.value == "") {
+        alert("Meme Owner field is empty");
+        nameInput.focus();
+        return false;
+    }
+    if (captionInput.value == "") {
+        alert("Caption field is empty");
+        caption.focus();
+        return false;
+    }
+    if (urlInput.value == "") {
+        alert("Meme URL field is empty");
+        urlInput.focus();
+        return false;
+    }
+    if(!is_url(urlInput.value)){
+        alert("Meme URL is not an url");
+        urlInput.focus();
+        return false;
+    }
+    return true;
+}
+
 //make a post request to the server
 $("#submit").click(function () {
-    $.ajax({
-        url: serverURL,
-        type: 'POST',
-        data: {
-            name: nameInput.value,
-            url: urlInput.value,
-            caption: captionInput.value
-        },
-        statusCode: {
-            409: function (responseObject, textStatus, jqXHR) {
-                reset();
-                alert("Meme already exists");
+    if (validate()) {
+        $.ajax({
+            url: serverURL,
+            type: 'POST',
+            data: {
+                name: nameInput.value,
+                url: urlInput.value,
+                caption: captionInput.value
+            },
+            statusCode: {
+                409: function (responseObject, textStatus, jqXHR) {
+                    reset();
+                    alert("Meme already exists");
+                }
             }
-        }
-    }).done(function (data) {
-        reset();
-        loadMemes();
-    })
+        }).done(function (data) {
+            reset();
+            loadMemes();
+        })
+    }
 });
 
 //add listeners to the newly created memes
